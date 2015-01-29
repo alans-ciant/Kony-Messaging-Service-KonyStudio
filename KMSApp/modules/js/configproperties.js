b720cda9ebeecfbba624b41fd1414a9e850def35
KonyDev <platform_integration_team@kony.com>
2015-01-29 15:27:31 +0530

    Update configproperties.js

--- KMSApp/modules/js/configproperties.js
+++ KMSApp/modules/js/configproperties.js
@@ -2,15 +2,13 @@
 /*
 var KMSPROP = {
 
-		kmsserverurl : "<Enter your messagign cloud instance url...>", // https://mycloud.messaging.konycloud.com
+		kmsserverurl : "<Enter your messagign cloud instance url...>", //mycloud.messaging.konycloud.com
 		senderID :"<Enter your senderID..>", // applicable for android only- clientID generated in google console
 		appID:"<Enter your app id...>", // appID generated or given in the cloud KMS console
-		deviceID : kony.os.deviceInfo().deviceid,
-		ufID:"<Enter your UFID ...>", // any unique identifier for the app
 		
 };*/
 KMSPROP = {
-		kmsserverurl:"mobilefabric-demo.messaging.konycloud.com",
+		kmsserverurl:"mobilefabric-demo.messaging.konycloud.com",//messaging service cloud url.
 		senderID :"208976063116", // applicable for android only- clientID generated in google console
 		appId:"17797-4209187586" // appID generated or given in the cloud KMS console
 };
